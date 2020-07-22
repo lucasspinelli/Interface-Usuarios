0772883f69ab877d9b77db6d786538f413ae70d4
Lucas Levi <lucas.lsl12@gmail.com>
2020-07-21 22:02:59 -0300

    Criando algumas variáveis, buscando elementos e começando a configurar o primeiro

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-alert("hello World!");
+let name = document.querySelector("exampleInputName");
+let gender = document.querySelectorAll("#form-user-create [name=gender]:checked");// Pseudo Seletor, retorna somente os 'checked'
+let birth = document.querySelector("exampleInputBirth");
+let country = document.querySelector("exampleInputCountry");
+let email = document.querySelector("exampleInputEmail");
+let password = document.querySelector("exampleInputPassword");
+let photo = document.querySelector("exampleInputFile");
+let admin = document.querySelector("#exampleInputAdmin");
+
+
+let fields = document.querySelectorAll("#form-user-create [name]"); //trazer todos os campos que tem name
+var user = {};
+fields.forEach(function(field, index){ //encontrou o campo nome? Executa o código
+
+    if (field.name == "gender"){ // == compara o valor === compara valor e tipo
+
+        if(field.checked){  // Se não escrever nada, compara com True
+            user[field.name] = field.value;
+        } 
+
+    } else {
+
+        user[field.name] = field.value;
+
+    }
+
+});
+
+console.log(user);
