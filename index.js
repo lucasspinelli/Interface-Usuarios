f672a67f49d701f4a65cf130544dd94d21901385
Lucas Levi <lucas.lsl12@gmail.com>
2020-07-22 11:10:34 -0300

    Adicionando o primeiro evento a página

--- index.js
+++ index.js
@@ -10,20 +10,29 @@ let admin = document.querySelector("#exampleInputAdmin");
 
 let fields = document.querySelectorAll("#form-user-create [name]"); //trazer todos os campos que tem name
 var user = {};
-fields.forEach(function(field, index){ //encontrou o campo nome? Executa o código
 
-    if (field.name == "gender"){ // == compara o valor === compara valor e tipo
-
-        if(field.checked){  // Se não escrever nada, compara com True
-            user[field.name] = field.value;
-        } 
-
-    } else {
-
-        user[field.name] = field.value;
-
-    }
-
-});
 
 console.log(user);
+
+document.getElementById("form-user-create").addEventListener("submit", function(event){
+
+    event.preventDefault(); //cancelar o comportamento padrão
+
+    fields.forEach(function(field, index){ //encontrou o campo nome? Executa o código
+
+        if (field.name == "gender"){ // == compara o valor === compara valor e tipo
+    
+            if(field.checked){  // Se não escrever nada, compara com True
+                user[field.name] = field.value;
+            } 
+    
+        } else {
+    
+            user[field.name] = field.value;
+    
+        }
+    });
+
+        console.log(user);
+   
+}); 
