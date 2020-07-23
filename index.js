d9cea4bbfd8a12ec06a3c853d00638e46a4ce600
Lucas Levi <lucas.lsl12@gmail.com>
2020-07-22 21:10:37 -0300

    Criando a primeira função, usando Template String e corrigindo o HTML

--- index.js
+++ index.js
@@ -11,6 +11,27 @@ let admin = document.querySelector("#exampleInputAdmin");
 let fields = document.querySelectorAll("#form-user-create [name]"); //trazer todos os campos que tem name
 var user = {};
 
+function addLine(dataUser){
+
+    console.log("add Line", dataUser);
+
+    let tr = document.createElement("tr");
+    tr.innerHTML =/* `` == Template String*/ ` 
+        <tr>
+            <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        </tr>
+     `;
+     document.getElementById("table-users").appendChild(tr);
+}
+
 
 console.log(user);
 
@@ -33,6 +54,7 @@ document.getElementById("form-user-create").addEventListener("submit", function(
         }
     });
 
-        console.log(user);
+        addLine(user);
    
 }); 
+
