96b429fdd604947aa2296136187d366371884b3f
Lucas Levi <lucas.lsl12@gmail.com>
2020-07-23 21:26:29 -0300

    Corrigindo erro de innerHTML usando o appendChild e deixando a visualização de adm mais amigável ao usuário

--- constrollers/userController.js
+++ constrollers/userController.js
@@ -63,9 +63,12 @@ class UserController  {
 
             };
 
-            fileReader.readAsDataURL(file);
-
-
+            if (file){
+            
+                fileReader.readAsDataURL(file); 
+            } else {
+                resolve('dist/img/boxed-bg.jpg');
+            }
         });
 
         
@@ -83,7 +86,11 @@ class UserController  {
                     user[field.name] = field.value;
                 } 
         
-            } else {
+            } else if (field.name === "admin") {
+
+                user[field.name] = field.checked;
+
+            } else  {
         
                 user[field.name] = field.value;
         
@@ -105,20 +112,24 @@ class UserController  {
     }
 
     addLine(dataUser){
+
+        let tr = document.createElement("tr");
+
+        tr.innerHTML = /* `` == Template String*/ ` 
+
+            <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${(dataUser.admin)? 'Sim' : 'Não'}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        
+     `;
     
-        this.tableEl.innerHTML =/* `` == Template String*/ ` 
-            <tr>
-                <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
-                <td>${dataUser.name}</td>
-                <td>${dataUser.email}</td>
-                <td>${dataUser.admin}</td>
-                <td>${dataUser.birth}</td>
-                <td>
-                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                </td>
-            </tr>
-         `;
+        this.tableEl.appendChild(tr);
        
     }
 
