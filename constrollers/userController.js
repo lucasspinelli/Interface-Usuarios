1a9656221e19051b72cfab0d1c9b1d2c65cac2b1
Lucas Levi <lucas.lsl12@gmail.com>
2020-07-28 22:30:02 -0300

    Adicionando For In para selecionar os elementos a serem alterados no form

--- constrollers/userController.js
+++ constrollers/userController.js
@@ -165,7 +165,26 @@ class UserController  {
 
         tr.querySelector(".btn-edit").addEventListener("click", e=>{ //evento ao clicar "editar"
 
-          console.log(JSON.parse(tr.dataset.user));  
+          let json = JSON.parse(tr.dataset.user);
+          
+          let form =  document.querySelector("#form-user-update");
+
+          for (let name in json){
+
+           let field = form.querySelector("[name=" + name.replace("_", "") + "]");
+
+
+           if(field){
+
+                if (field.type =='file') continue; // continue = ignora tudo pra baixo e vai pro próximo
+
+                field.value = json[name];
+
+           }
+
+           
+
+          }
 
           this.showPanelUpdate();
 
